ccce74953c3e76389308f3f106832b20abd30dd5
Caleb Sharp <epicsharp11@gmail.com>
2016-05-03 15:51:29 -0700

    Add counter actions

---
+++ src/actions/counter.js
@@ -0,0 +1,10 @@
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
+export const incrementCounter = () => ({
+  type: INCREMENT_COUNTER
+})
+
+export const decrementCounter = () => ({
+  type: DECREMENT_COUNTER
+})
