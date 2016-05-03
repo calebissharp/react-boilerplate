1afcbb09b4c4d46265360e698a6f653f174b5f08
Caleb Sharp <epicsharp11@gmail.com>
2016-05-02 19:30:34 -0700

    Add server entry point

---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require('./server')
+
+const port = process.env.port || 8080
+const host = process.env.IP || '0.0.0.0'
+
+server.listen(port, host, () => {
+  console.log('Server listening on http://' + host + ':' + port + '/');
+})
