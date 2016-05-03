1ecf8960a86dd5118e84bcbcec73285e2894bcf4
Caleb Sharp <epicsharp11@gmail.com>
2016-05-02 20:36:27 -0700

    Move index.html (again)

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 module.exports = app
