ef62e89b10f969a37ce054e5bf1b99d4f039b0d6
Caleb Sharp <epicsharp11@gmail.com>
2016-05-02 20:43:27 -0700

    Capitalize constants

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const server = require('./server')
 
-const port = process.env.PORT || 8080
-const host = process.env.IP || '0.0.0.0'
+const PORT = process.env.PORT || 8080
+const HOST = process.env.IP || '0.0.0.0'
 
-server.listen(port, host, () => {
-  console.log('Server listening on http://' + host + ':' + port + '/')
+server.listen(PORT, HOST, () => {
+  console.log('Server listening on http://' + HOST + ':' + PORT + '/')
 })
