92f4ab24edf33f81fcffcda7a80b974ae85fd17a
Caleb Sharp <epicsharp11@gmail.com>
2016-07-26 16:15:49 -0600

    Remove node.js server in favour of webpack-dev-server CLI

--- index.js
+++
@@ -1,8 +0,0 @@
-const server = require('./server')
-
-const PORT = process.env.PORT || 8080
-const HOST = process.env.IP || '0.0.0.0'
-
-server.listen(PORT, HOST, () => {
-  console.log('Server listening on http://' + HOST + ':' + PORT + '/')
-})
