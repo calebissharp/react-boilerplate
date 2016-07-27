b8bc52dfb8e4a6ff044c616133b7b1bc3317947f
Caleb Sharp <epicsharp11@gmail.com>
2016-07-27 09:23:29 -0600

    Pass the store to the components with 'Provider'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import configureStore from './configureStore'
 import App from './components/App'
 
 const store = configureStore()
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('react')
 )
