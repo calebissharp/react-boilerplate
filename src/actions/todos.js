205d41f72495d8630c4b6e137a5a8c2bc35473b7
Caleb Sharp <epicsharp11@gmail.com>
2016-07-30 17:42:21 -0600

    Add the todos actions

---
+++ src/actions/todos.js
@@ -0,0 +1,40 @@
+export const CREATE_TODO = 'CREATE_TODO'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const COMPLETE_TODO = 'COMPLETE_TODO'
+export const REQUEST_TODOS = 'REQUEST_TODOS'
+export const RECEIVE_TODOS = 'RECEIVE_TODOS'
+
+export const createTodo = (title) => ({
+  type: CREATE_TODO,
+  complete: false,
+  title
+})
+
+export const removeTodo = (id) => ({
+  type: REMOVE_TODO,
+  id
+})
+
+export const completeTodo = (id) => ({
+  type: COMPLETE_TODO,
+  id
+})
+
+export const requestTodos = () => ({
+  type: REQUEST_TODOS
+})
+
+export const receiveTodos = (json) => ({
+  type: RECEIVE_TODOS,
+  todos: json.todos,
+  receivedAt: Date.now()
+})
+
+export const fetchTodos = () => {
+  return dispatch => {
+    dispatch(requestTodos())
+    return fetch('http://localhost:8080/todos.json')
+      .then(res => res.json())
+      .then(json => dispatch(receiveTodos(json)))
+  }
+}
