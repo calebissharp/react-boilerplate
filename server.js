faa0b2cea3006779b7fcb8c1ae822521b7103782
Caleb Sharp <epicsharp11@gmail.com>
2016-07-30 13:45:26 -0600

    Reverse OR

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express()
 
 app.use(express.static('public'))
 
-const PORT = 8080 || process.env.port
-const HOST = '0.0.0.0' || process.env.host
+const PORT = process.env.port || 8080
+const HOST = process.env.host || '0.0.0.0'
 
 app.listen(PORT, HOST, () => {
   console.log(`App listening on http://${HOST}:${PORT}/`)
