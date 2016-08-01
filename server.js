9f92d43d4835f0c2e15cb3c17942679e6d49d5ed
Caleb Sharp <epicsharp11@gmail.com>
2016-08-01 01:18:15 -0600

    Fix the server to work with react-router

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'))
+})
 
 const PORT = process.env.port || 8080
 const HOST = process.env.host || '0.0.0.0'
