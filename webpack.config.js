8cce9f1be938d95723fe4447ff6dbfbec375c676
Caleb Sharp <epicsharp11@gmail.com>
2016-08-01 00:51:46 -0600

    Add process.env

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   devtool: '#eval-source-map',
@@ -27,6 +28,13 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ],
   devServer: {
     historyApiFallback: true
   }
