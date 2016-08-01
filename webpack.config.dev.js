1bf6483e10cbf20c255275625bd2bb6c98f4d4f7
Caleb Sharp <epicsharp11@gmail.com>
2016-08-01 01:50:05 -0600

    Fix the dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,21 @@
-const config = require('./webpack.config.base')
+const { config, loaders } = require('./webpack.config.base')
 
 const devConfig = Object.assign({}, config, {
   devtool: '#eval-source-map',
   devServer: {
     historyApiFallback: true
+  },
+  module: {
+    loaders: [
+      loaders.cssLoader,
+      loaders.sassLoader,
+      loaders.assetLoader,
+      loaders.imageLoader,
+      Object.assign({}, loaders.jsLoader, {
+        loaders: ['react-hot', 'babel']
+      })
+    ]
   }
 })
-
-devConfig.module.loaders[0].loaders = ['react-hot', 'babel']
 
 module.exports = devConfig
