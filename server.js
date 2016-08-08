5af2feb23614c203f6db0311835e7502e57531c8
Caleb Sharp <epicsharp11@gmail.com>
2016-08-08 12:00:46 -0700

    Uppercase environment variables

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get('/*', (req, res) => {
   res.sendFile(path.resolve('dist/index.html'))
 })
 
-const PORT = process.env.port || 8080
-const HOST = process.env.host || '0.0.0.0'
+const PORT = process.env.PORT || 8080
+const HOST = process.env.HOST || '0.0.0.0'
 
 app.listen(PORT, HOST, () => {
   console.log(`App listening on http://${HOST}:${PORT}/`)
