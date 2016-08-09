dfb800c333a0e3687bd542f30b3524282eb62811
Caleb Sharp <epicsharp11@gmail.com>
2016-08-09 13:33:16 -0700

    Remove unneeded async action

--- src/actions/counter.js
+++ src/actions/counter.js
@@ -1,6 +1,5 @@
 export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
 export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
-export const INCREMENT_COUNTER_ASYNC = 'INCREMENT_COUNTER_ASYNC'
 
 export const incrementCounter = () => ({
   type: INCREMENT_COUNTER
