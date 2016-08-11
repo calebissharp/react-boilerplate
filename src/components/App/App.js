6023d2a8d8dc4577a874849f51d295b3b6afab67
Caleb Sharp <epicsharp11@gmail.com>
2016-08-11 12:37:40 -0700

    Remove navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,4 @@
 import React, { PropTypes, Component } from 'react'
-import { LinkContainer } from 'react-router-bootstrap'
-import { Link } from 'react-router'
-import { Nav, NavItem, Navbar } from 'react-bootstrap'
 
 import DevTools from '../../containers/DevTools'
 
@@ -9,25 +6,6 @@ class App extends Component {
   render () {
     return (
       <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">React Boilerplate</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <LinkContainer to={{pathname: 'counter'}}>
-                <NavItem>Counter</NavItem>
-              </LinkContainer>
-              <LinkContainer to={{pathname: 'todos'}}>
-                <NavItem>Todos</NavItem>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
         {this.props.children}
 
         {process.env.NODE_ENV !== 'production' &&
